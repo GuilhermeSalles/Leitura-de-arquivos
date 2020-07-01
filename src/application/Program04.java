e712c8eebaec593179f13c01ce3daa16f81f748b
GuilhermeSalles <guibaltazarvs@gmail.com>
2020-07-01 12:25:09 -0300

    Informações do caminho do arquivo

--- src/application/Program04.java
+++ src/application/Program04.java
@@ -1,9 +1,24 @@
 package application;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Program04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
+
+		File path = new File(strPath);
+
+		System.out.println("getPath: " + path.getPath());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
+
+		sc.close();
 
 	}
 
